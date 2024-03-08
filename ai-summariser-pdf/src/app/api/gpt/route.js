66f6afc167819afe388909695d44df3e703b53d5
mrud0004 <110021431+mrud0004@users.noreply.github.com>
2024-03-08 21:33:15 +1100

    update git ignore

--- ai-summariser-pdf/src/app/api/gpt/route.js
+++ ai-summariser-pdf/src/app/api/gpt/route.js
@@ -1,7 +1,17 @@
 import { NextResponse } from "next/server";
+import OpenAI from 'openai';
 
 
 export async function POST(request) {
+
+
+    const openai = new OpenAI({
+       apiKey =  
+    })
+
+
+
+
   return NextResponse.json({ 
    name: "Hello, World!" 
 });
