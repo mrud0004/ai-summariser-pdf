287a000b0d230d574b6b661b68f0c32bf5952510
mrud0004 <110021431+mrud0004@users.noreply.github.com>
2024-03-08 21:47:56 +1100

    added chatgpt to route.js
    added functionality to route.js to get chat gpt giving repsonses.

--- ai-summariser-pdf/src/app/api/gpt/route.js
+++ ai-summariser-pdf/src/app/api/gpt/route.js
@@ -4,15 +4,20 @@ import OpenAI from 'openai';
 
 export async function POST(request) {
 
-
+    console.log(process.env.OPENAI_API_KEY)
     const openai = new OpenAI({
-       apiKey =  
+       apiKey:process.env.OPENAI_API_KEY
     })
 
 
+   const completion = await openai.chat.completions.create({
+    messages: [{ role: "system", content: "You are a helpful assistant." },
+    {role:"user",content: "what is the meaning of life?"}
+],
+    model: "gpt-3.5-turbo",
+  });
 
+  console.log("Completion",completion.choices[0])
 
-  return NextResponse.json({ 
-   name: "Hello, World!" 
-});
+  return NextResponse.json(completion.choices[0]);
 }
